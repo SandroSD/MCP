7052d8bbc286c5dc85f951027bdcaeb46c0cc044
Sandro Dezerio <sdezerio@gmail.com>
2025-09-23 07:55:00 -0500

    move to folder.

---
+++ example-01/src/index.ts
@@ -0,0 +1,29 @@
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+
+import { getAlertsTool } from "./tools/getAlerts.js";
+import { getForecastTool } from "./tools/getForecast.js";
+
+// Create server instance
+const server = new McpServer({
+  name: "weather",
+  version: "1.0.0",
+  capabilities: {
+    resources: {},
+    tools: {},
+  },
+});
+
+getAlertsTool(server);
+getForecastTool(server);
+
+async function main() {
+  const transport = new StdioServerTransport();
+  await server.connect(transport);
+  console.error("Weather MCP Server running on stdio");
+}
+
+main().catch((error) => {
+  console.error("Fatal error in main():", error);
+  process.exit(1);
+});
